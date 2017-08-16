1b45557cf91627b1f14b62806e7f45ec370438ad
Jan Korona <jan.korona@blstream.com>
2017-08-16 15:32:52 +0200

    delete unneeded data

--- ios/Pods/Target Support Files/Pods-pluses-tvOS/Pods-pluses-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_pluses_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_pluses_tvOS
-@end
--- ios/Pods/Target Support Files/Pods-pluses-tvOSTests/Pods-pluses-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_pluses_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_pluses_tvOSTests
-@end
